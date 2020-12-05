42fadbe78288753de68b57b98f98eae6d050587f
Vismark Juarez <vismarkjuarez@gmailcom>
2020-12-05 02:07:14 -0600

    Removed numeric portion from the address

--- project.py
+++ project.py
@@ -1,6 +1,9 @@
 "#!/usr/bin/env python3"
 import sys
 import pandas as pd
+from pandas import DataFrame
+
+pd.set_option('display.max_columns', None)
 
 
 def extract_target_values(df, column_name, relevant_target_values):
@@ -36,12 +39,48 @@ def clean_dataset(data_frame):
     '''
     Removing all columns that do not provide any relevant insight into the data:
     '''
-    print(data_frame.describe())
     data_frame = data_frame.drop(columns=["ID", "Case Number", "IUCR", "Beat", "District", "Community Area", "FBI Code", "X Coordinate", "Y Coordinate", "Year", "Updated On", "Latitude", "Longitude", "Location"], axis=1)
 
-    # store the filtered dataset (not necessary, but useful  JiC).
-    data_frame.to_csv(index=False, path_or_buf="data/filtered-crime-data.csv")
     return data_frame
+
+
+def strip_block_numbers(data_frame):
+    # extract the 'Block' column
+    block_df = data_frame["Block"]
+
+    # convert the one-dimension df to a list
+    block_list = block_df.values.tolist()
+
+    # iterate through each item in the list and remove the first 6 elements of the string (the numeric address portion)
+    i = 0
+    updated_block_list = []
+
+    for address in block_list:
+        updated_block_list.append(address[6:]) # split at 6th index, all the way till the end of the string
+        i = i + 1  # increment the stupid counter
+
+    # remove the current 'Block' column from the received data_frame
+    data_frame = data_frame.drop(columns=["Block"], axis=1)
+
+    # combine the processed_block_df to the data_frame
+    data_frame['Block'] = updated_block_list
+
+    return data_frame
+
+
+def encode_block(data_frame):
+    return data_frame
+
+
+def preprocess(data_frame):
+    # remove unecessary address numbers
+    data_frame = strip_block_numbers(data_frame)
+
+    # on-hot-encode the Block values
+    data_frame = encode_block(data_frame)
+
+    return data_frame
+
 
 def main():
     # Keeping name upper-cased to let the world know this var shouldn't be changed
@@ -50,7 +89,13 @@ def main():
     df = filter_dataset(RAW_DATA_PATH)
     df = clean_dataset(df)
 
-    print(df)
+    # Perform any preprocessing logic
+    df = preprocess(df)
+
+    # store the filtered dataset (not necessary, but useful  JiC).
+    df.to_csv(index=False, path_or_buf="data/filtered-crime-data.csv")
+
+    print(df.head())
 
 
 if __name__ == "__main__":
