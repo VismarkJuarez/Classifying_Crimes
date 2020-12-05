dd8834de9cb25388c32a047781decce16397de36
Vismark Juarez <vismarkjuarez@gmailcom>
2020-12-04 22:52:03 -0600

    Removed unecessary columns from the dataset.

--- project.py
+++ project.py
@@ -28,20 +28,29 @@ def filter_dataset(raw_data_path):
     relevant_target_values = ["BATTERY", "THEFT", "CRIMINAL DAMAGE"]  # can be changed for experimentation
     df = extract_target_values(raw_df, column_name, relevant_target_values)  # filtering the raw dataset
 
-    # store the filtered dataset (not necessary, but useful  JiC).
-    df.to_csv(index=False, path_or_buf="data/filtered-crime-data.csv")
-
     return df  # returning the filtered dataframe
 
+
+def clean_dataset(data_frame):
+    #Drop 'Case number'
+    '''
+    Removing all columns that do not provide any relevant insight into the data:
+    '''
+    print(data_frame.describe())
+    data_frame = data_frame.drop(columns=["ID", "Case Number", "IUCR", "Beat", "District", "Community Area", "FBI Code", "X Coordinate", "Y Coordinate", "Year", "Updated On", "Latitude", "Longitude", "Location"], axis=1)
+
+    # store the filtered dataset (not necessary, but useful  JiC).
+    data_frame.to_csv(index=False, path_or_buf="data/filtered-crime-data.csv")
+    return data_frame
 
 def main():
     # Keeping name upper-cased to let the world know this var shouldn't be changed
     RAW_DATA_PATH = "data/filtered-crime-data.csv"
 
     df = filter_dataset(RAW_DATA_PATH)
+    df = clean_dataset(df)
 
-    print(df.describe())
-    print(len(df))
+    print(df)
 
 
 if __name__ == "__main__":
